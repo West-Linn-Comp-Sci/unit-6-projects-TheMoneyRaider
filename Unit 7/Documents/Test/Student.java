f5f82347faeda43964a3ff4500dda93c2cc82475
TheMoneyRaider <83470534+TheMoneyRaider@users.noreply.github.com>
2021-05-21 12:09:15 -0700

    Delete Unit 7 directory

--- Unit 7/Documents/Test/Student.java
+++
@@ -1,8 +0,0 @@
-public class Student extends Person { 
-    private int myGrade; 
-    public Student() { myGrade = 9;} 
-    public void setGrade(int grade) { myGrade = grade;} 
-    public int getGrade() { return myGrade;} 
-    public void passExams() {myGrade++;} 
-    public void saySomething() {System.out.println("I am a student");} 
-} 
